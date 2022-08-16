b0282122619a61020821be962f0e7c27b67ceeab
Maxi Portillo <maxinportillo@gmail.com>
2022-08-16 12:35:30 -0300

    ejercicio n1

--- promedio.py
+++ promedio.py
@@ -1,1 +1,7 @@
-nota_1 = input("inserte nota")
+nota_1 = int(input("Ingrese la primer nota: "))
+nota_2 = int(input("Ingrese la segunda nota: "))
+nota_3 = int(input("Ingrese la tercer nota: "))
+
+calculo_promedio = str((nota_1 * 0.1 + nota_2 * 0.3 + nota_3 * 0.5) / (0.2 + 0.3 + 0.5))
+
+mostrar_promedio = print("El promedio ponderado de las notas es: " + calculo_promedio)
