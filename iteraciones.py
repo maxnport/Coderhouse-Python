98666e54585eecc357b8164fed2c1653305cc650
Maxi Portillo <maxinportillo@gmail.com>
2022-08-16 12:19:16 -0300

    Borrado archivos antiguos

--- iteraciones.py
+++
@@ -1,8 +0,0 @@
-estudiantes = {
-    'mexico': 10,
-    'colombia': 15,
-    'puerto_rico': 4,
-}
-
-for pais in estudiantes:
-    print(pais)
--- jira.py
+++
@@ -1,2 +0,0 @@
-usuario = input("Ingrese nombre: ")
-mensaje = print("Hola " + usuario + ", cómo te va? Te pido que por favor cargues la petición en JIRA (caurs.prhge.com) y en cuanto pueda lo resolvemos. Gracias!")
